a59238f4ed79632855574c3addd7c7705da6215c
johnlaschober <johnlaschober@gmail.com>
2018-04-25 21:03:09 -0500

    Filter switch case setup

--- IFP.js
+++ IFP.js
@@ -1,24 +1,19 @@
- function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-
-                reader.onload = function (e) {
-                    $('#imgview')
-                        .attr('src', e.target.result);
-                };
-
-                reader.readAsDataURL(input.files[0]);
-				reader.crossOrigin = "Anonymous";
-            }
-        }
-		
-function makeGray()
+function readURL(input) 
 {
-	document.getElementById("imgview").style.filter = "grayscale(100%)";
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+
+		reader.onload = function (e) {
+			$('#imgview')
+				.attr('src', e.target.result);
+		};
+
+		reader.readAsDataURL(input.files[0]);
+	}
 }
 
-function editCanvas()
-{
+function editCanvas(val)
+{	
 	var canvas = document.getElementById('myCanvas'),
 		context = canvas.getContext('2d');
 	var img = document.getElementById("imgview");
@@ -36,29 +31,42 @@ function editCanvas()
 	{
 		var r = pix[i], g = pix[i+1], b = pix[i+2];
 		
-		// Sepia formula
-		pix[i] = (r*.393)+(g*.769)+(b*.189);
-		pix[i+1] = (r*.394)+(g*.686)+(b*.168);
-		pix[i+2] = (r*.272)+(g*.534)+(b*.131);
+		switch(val)
+		{
+			case "NoEdit":
+			pix[i] = r;
+			pix[i+1] = g;
+			pix[i+2] = b;
+			break;
+			case "Jacob":
+			break;
+			case "Thaddeus":
+			break;
+			case "Jace":
+			break;
+			case "Edwin":
+			break;
+			case "Michael":
+			break;
+			case "Cris":
+			break;
+			case "John":
+			// Sepia formula
+			pix[i] = (r*.393)+(g*.769)+(b*.189);
+			pix[i+1] = (r*.394)+(g*.686)+(b*.168);
+			pix[i+2] = (r*.272)+(g*.534)+(b*.131);
+			break;
+			case "Nicolas":
+			break
+			case "Ali":
+			break;
+			case "Ahmad":
+			break;
+			case "Karol":
+			break;
+		}
 	}
 	context.putImageData(imageData, 0, 0);
-	
-	var download = document.getElementById("test");
 
-	if (document.getElementById("myCanvas"))
-	{
-		console.log("Found");
-	}
-	else
-	{
-		console.log("Not found");
-	}
 	var savedImage = document.getElementById("myCanvas").toDataURL("image/png").replace("image/png", "image/octet-stream");
-	download.href = savedImage;
-	console.log(img.name);
-	download.download = "test.png";
-	
-
-	
-	// Can we copy pixels from img to canvas pixels?
 }
